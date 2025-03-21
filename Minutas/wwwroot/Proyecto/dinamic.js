ac734bb6199565ea04602e395f670a178677c294
González Rojas Karla Guadalupe <211G0353@rcarbonifera.tecnm.mx>
2025-03-20 23:27:20 -0600

    Vistas actualizadas en wwwroot, cambie algunos id por class :D

--- Minutas/wwwroot/Proyecto/dinamic.js
+++ Minutas/wwwroot/Proyecto/dinamic.js
@@ -1,4 +1,4 @@
-let modal = document.querySelector("#modal");
+/*let modal = document.querySelector("#modal");
     let eliminar = document.querySelector("table");
     eliminar.addEventListener("click", function(e){
         if(e.target.tagName === "TD" && e.target.id === "eliminar"){
@@ -10,4 +10,25 @@ let modal = document.querySelector("#modal");
             modal.style.display = "none";
         }
         
+    });*/
+/*
+    let nav = document.querySelector("nav");
+    nav.addEventListener("click", function(e){
+        if(e.target.tagName == "A"){
+            let div = e.target.parentElement;
+            let color = "blue";
+    
+            localStorage.setItem("pestanaSeleccionada", e.target.href);
+            localStorage.setItem("colorFondo", nuevoColor);
+    
+            window.location.href = e.target.href;
+        }
+    });*/
+
+    let nav = document.querySelector("nav a");
+    let div = document.querySelector("nav div");
+    nav.addEventListener("click", function(e){
+        if(e.target.tagName == "A"){
+            div.style.background="blue";
+        }
     });
