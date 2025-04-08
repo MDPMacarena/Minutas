9463725f5c0ff49263fa7de5a0aa55eab12dba0a
211G0365 <126606377+211G0365@users.noreply.github.com>
2025-04-08 07:40:38 -0600

    Inicio de Sesion

--- Minutas/Controllers/HomeController.cs
+++ Minutas/Controllers/HomeController.cs
@@ -1,13 +1,68 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Minutas.Models;
+using System.Security.Claims;
 
 namespace Minutas.Controllers
 {
     public class HomeController : Controller
     {
+        public HomeController(DbminutasContext context)
+        {
+            Context = context;
+        }
+
+        public DbminutasContext Context { get; }
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
-        //funciona
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string rol, string password)
+        {
+
+            var usuario = Context.Usuarios
+                .SingleOrDefault(x => x.NumEmpleado == rol && x.ContraseñaHash == password && x.Activo == true);
+
+            if (usuario != null)
+            {
+                var rolUsuario = usuario.IdRolNavigation.Nombre;
+
+
+                List<Claim> claims = new List<Claim>
+                {
+                new Claim("Id", usuario.Id.ToString()),
+                 new Claim(ClaimTypes.Role, rolUsuario)
+                };
+
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                await HttpContext.SignInAsync(new ClaimsPrincipal(identity));
+
+
+                if (rolUsuario == "Admin")
+                {
+                    return RedirectToAction("Index", "Home", new { area = "Admin" });
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+
+            ModelState.AddModelError("", "El usuario o contraseña son incorrectos o el usuario está inactivo");
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect("/");
+        }
+
     }
 }
