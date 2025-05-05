cb9e1bc297453019ed4c664419ae3f6884b1849f
Duarte Angeles Evelyn Azeneth <211G0891@rcarbonifera.tecnm.mx>
2025-05-05 07:22:17 -0600

    departamentos

--- Minutas/Program.cs
+++ Minutas/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMvc();
 // Configura el contexto de la base de datos
 builder.Services.AddDbContext<Minutas.Models.DbminutasContext>(options =>
-    options.UseMySql("server=localhost;user=root;password=root;database=dbminutas", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.1.0-mysql")));
+    options.UseMySql("server=localhost;user=root;password=root;database=dbminutas;port=3307", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.1.0-mysql")));
 
 // Agrega esta línea para registrar los repositorios
 builder.Services.AddScoped<DepartamentoRepository>();
