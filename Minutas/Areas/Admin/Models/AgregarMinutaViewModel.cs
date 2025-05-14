2b30b9c3546cfe21705279ae14d8aec3e30657a6
Luis <211g0373@rcarbonifera.tecnm.mx>
2025-05-14 07:14:17 -0600

    AgregarMinuta

---
+++ Minutas/Areas/Admin/Models/AgregarMinutaViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MinutasManage.Models;
+
+namespace MinutasManage.Areas.Admin.Models
+{
+    public class AgregarMinutaViewModel
+    {
+
+        public Minutas Minuta { get; set; } = new Minutas();
+        public List<Usuarios> Usuarios { get; set; } = new List<Usuarios>();
+        public List<string> Externos { get; set; } = new List<string>();
+
+
+    }
+}
